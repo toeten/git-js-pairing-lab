86a4f8f231efaf57d638910c825b6ca87e814639
mekhitudor <mekhictudor@gmail.com>
2024-03-27 15:56:59 -0400

    question two answered

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const fiveToOneHundred = () => {
     }
 }
 fiveToOneHundred()
+
+const multiplesOfThree = () => {
+    for (let i = 0; i < 101; i++) {
+        console.log(i * 3)
+    }
+}
+
+multiplesOfThree()
