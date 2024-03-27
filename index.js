b25cc6cb1c0375c32d096958a16a4480406c7bfa
toeten <n.santiago0228@gmail.com>
2024-03-27 16:03:29 -0400

    answered question 3

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ const multiplesOfThree = () => {
 }
 
 multiplesOfThree()
+
+const multiplesOfThreeOrFive = () => {
+    for (let i = 1; i < 101; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i)
+        }
+    }
+}
+multiplesOfThreeOrFive()
